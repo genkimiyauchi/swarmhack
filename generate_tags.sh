9957599412213870663e2096530fd0638b9d421b
Genki Miyauchi <g.miyauchi@sheffield.ac.uk>
2026-02-03 17:13:31 +0000

    Delete mona related code

--- generate_tags.sh
+++ generate_tags.sh
@@ -17,18 +17,3 @@ do
         rm -rf DICT_*
     fi
 done
-
-# 30 unique MONA IDs
-for i in {31..60}
-do
-    echo $i, $page
-
-    python3 generate_tag.py --id $i --diameter 80
-
-    if [ $((i % tags_per_page)) -eq 0 ]
-    then
-        montage -geometry +2+2 DICT_* $page.png
-        ((page++))
-        rm -rf DICT_*
-    fi
-done
